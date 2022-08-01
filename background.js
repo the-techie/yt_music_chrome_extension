61bc1c2a399f5909e7e8f1804f169712d4878567
the-techie <dhirender.programmer@gmail.com>
2022-08-01 23:06:00 +0530

    Nearly complete

--- background.js
+++ background.js
@@ -5,22 +5,31 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
     // console.log("------");
 
     url = tab.url;
+    // console.log("tab:\t", tab);
+
+    // console.log("tabid, tab.tabid:\t", tabId, tab.id);
 
     if(url && url.includes("youtube.com/watch")){
-        const urlParam =  url.split("?")[1];
 
+        console.log("url:\t", url)
+
+        const urlParam =  url.split("?")[1];
+        // console.log("urlParams:\t", urlParam);
         urlQueryParams = new URLSearchParams(urlParam);
 
         const videoId = urlQueryParams.get("v");
         const listId = urlQueryParams.get("list");
-        // console.log("videoID:\t", videoID);
+        console.log("videoID:\t", videoId);
+        console.log("tabId:\t", tabId);
         // console.log("in background.js");
 
         // console.log(videoId)
-
+        console.log("Before send message");
         chrome.tabs.sendMessage(tabId, {
             "videoId" : videoId,
             "listId": listId,
+        }).catch((error)=>{
+            console.log("error:\t", error);
         });
     
     }
--- contentScript.js
+++ contentScript.js
@@ -1,14 +1,55 @@
 (() => {
-
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
         const {videoId, listId} = obj;
 
         console.log("videoID:\t", videoId);
         console.log("listId:\t", listId);
 
+        var obj = {};
+
+        obj[videoId] = "videoooooo";
+
+        chrome.storage.local.set(obj, ()=>{
+            console.log("written successfully");
+        })
+
+        chrome.storage.local.get([videoId], (result)=>{
+            console.log("value:\t", result);
+        });
+
+        response({"result": "Okay"});
+            
+        
+
+        
+        // setTimeout(printSongsInfo, 2000);
+        
+        
+        // title: t1.getElementsByClassName("song-title style-scope ytmusic-player-queue-item")[0].innerText
+        
         // id = queue
         // id = contents
-
+        // const songs_queue = document.getElementById("contents");
+        // return Promise.resolve("");
+        // return true;
     });
+    
+    // function printSongsInfo(){
+    //     all_songs = document.querySelectorAll("ytmusic-player-queue-item");
+        
+    //     console.log("allsongs", all_songs);
+        
+    //     // for(let song in all_songs)
+    //     // {
+    //     //     title = song.getElementsByClassName("song-title style-scope ytmusic-player-queue-item")[0].innerText;
+    //     //     console.log("title:\t", title);
+    //     //     names = song.getElementsByClassName("style-scope yt-formatted-string");
+
+    //     //     for(let name in names)
+    //     //         console.log(name.innerText);
+
+    //     // }
+
+    // }
 
 })();
